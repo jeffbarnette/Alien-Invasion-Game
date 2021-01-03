ddad6791c72ec839697567fdaf6f1486eeeeaf94
jeffbarnette <jwbarnette@icloud.com>
2021-01-03 17:36:30 -0600

    v1.0 Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Alien-Invasion-Game
 A simple 2D game written in Python and Pygame
+
+## Gameplay
+In Alien Invasion!, the player controls a rocket ship that appears at the bottom center of the screen. The player can move the ship right and left using the (R / L) arrow keys and shoot bullets using the (SPACEBAR). When the game begins, a fleet of aliens fills the sky and moves across and down the screen. The player shoots and destroys the aliens. If the player shoots all the aliens, a new fleet appears that moves faster than the previous fleet. If any alien hits the player's ship or reaches the bottom of the screen, the player loses a ship. If the player loses three ships, the game ends. The previous highest score is saved upon exiting the game.
+
+_All included music and sound effects are licensed under Creative Commons._
+
+### Setup and Installation
+- You will need Python 3.8.x or later
+
+`pip install -r requirements.txt`
+
+### Starting and Playing the Game
+
+`python3 alien_invasion.py`
+
+Click on the **Play** button to start a new game.
+
+Use the **Left** and **Right** arrow keys to move the ship and **Spacebar** to fire the laser cannon.
+
+To quit the game, press the **Q** key at any time.
+
+### Changelog
+
+**v1.0.0**
+- Initial Release
---
+++ button.py
@@ -0,0 +1,33 @@
+import pygame.font
+
+class Button:
+
+    def __init__(self, ai_game, message):
+        """Initialize button attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set the dimensions and properties of the button.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 160, 0) # Green
+        self.text_color = (255, 255, 255) # White
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Build the button's rect object and center it.
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        # The button message needs to be prepped only once.
+        self._prep_message(message)
+    
+    def _prep_message(self, message):
+        """Turn message into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(message, True, self.text_color,
+                self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+    
+    def draw_button(self):
+        # Draw blank button and then draw message.
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
